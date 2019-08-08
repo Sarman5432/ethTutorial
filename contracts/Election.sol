7ebae3c7ca495ed2388219a01f05629274332088
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2019-08-08 16:36:49 -0400

    Part 1

---
+++ contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    //store candidate
+    //read candidate
+    string public candidate;
+
+
+    //constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+    
+}
+
+
+
+
