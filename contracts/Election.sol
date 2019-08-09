819f577f0202cc937c3b232dcc9b0046eef74636
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2019-08-09 04:16:11 -0400

    Finished parts 1, 2, and some of 3

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,39 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-    //store candidate
-    //read candidate
-    string public candidate;
+    //model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
+    //store accounts that have voted
+    mapping(address => bool) public voters;
+     //store candidate
+    //fetch candidate
+    mapping(uint => Candidate) public candidates;
 
-    //constructor
+    //store candidiate count
+    uint public candidatesCount;
+
     constructor() public{
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+
+    }
+
+    function vote (uint _candidateId) public{
+        //record that voter has voted
+        voters[msg.sender] = true;
+
+        //update vote count
+        candidates[_candidateId].voteCount ++;
     }
     
 }
